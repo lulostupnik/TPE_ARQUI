94f3cf7cba5aa7a981ed68610fd755afd07117a4
Federico Viera <fedviera@gmail.com>
2024-05-20 12:03:12 -0300

    Started Syscalls.c

--- Kernel/syscalls.c
+++ Kernel/syscalls.c
@@ -1,11 +1,14 @@
 #include <stdint.h>
-
+#include <video.h>
 typedef struct {
     uint64_t r15, r14, r13, r12, r11, r10, r9, r8, rsi, rdi, rbp, rdx, rcx, rbx, rax;
 } Registers;
 
 void sysCallHandler(Registers * regs) {
-    
+    switch(regs->rax){
+        case 0: ;
+        case 1: ;
+    }
 }
 
 //ACA HAY QUE IMPLEMENTAR TODAS LAS INT 80h
