405f98f6dd9acf18da580570ad62407cebb8cde6
mrossisei <mrossiseifert@itba.edu.ar>
2024-05-30 00:13:15 -0300

    pinto

--- Userland/SampleCodeModule/Main/sampleCodeModule.c
+++ Userland/SampleCodeModule/Main/sampleCodeModule.c
@@ -12,8 +12,7 @@ static int var2 = 0;
 #define LAST_ASCII_CHAR 126
 int main() {
 
-    sys_write(1, "Hello World!\n", 13);
-
+    // sys_write(1, "Hello World!\n", 13);
     put_char('H');
 	//All the following code may be removed
     /*uint64_t a=sysWrite(0,0,"hola soy una vaca");
@@ -40,7 +39,7 @@ int main() {
 
     uint64_t c;
     uint64_t x = 0;
-    // initializeShell();
+    initializeShell();
 	//Test if BSS is properly set up
 	if (var1 == 0 && var2 == 0)
 		return 0xDEADC0DE;
