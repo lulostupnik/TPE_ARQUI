22c0759665ccbdbaec64115fb03b0b2066245d00
lulostupnik <lulostupnik2003@gmail.com>
2024-06-03 20:44:19 -0300

    deleted NC

--- Userland/SampleCodeModule/Main/sampleCodeModule.c
+++ Userland/SampleCodeModule/Main/sampleCodeModule.c
@@ -11,30 +11,6 @@ static int var2 = 0;
 #define FIRST_ASCII_CHAR 32
 #define LAST_ASCII_CHAR 126
 int main() {
-    // sys_write(1, "Hello World!\n", 13);
-    // put_char('H');
-	//All the following code may be removed
-    /*uint64_t a=sysWrite(0,0,"hola soy una vaca");
-    char string[2]={0,0};
-    string[0]=((a%10)+'0');
-    sysWrite(0,16,string);*/
-    /*
-    int x = 0;
-    char string[2] = {0,0};
-    getFirstKeyStroke();
-    while(1){
-        string[0] = getFirstKeyStroke();
-        if(string[0] != 0){
-            if( string[0] >= FIRST_ASCII_CHAR && string[0] <= LAST_ASCII_CHAR){
-                sysWrite(x,0,string);
-                x+=16;
-            }else{
-                //special keys handler
-            }
-
-        }
-    }
-    */
 
     uint64_t c;
     uint64_t x = 0;
