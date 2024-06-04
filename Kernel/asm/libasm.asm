4c26bc6e1f4efaac3435910ad0d0a79607702fe0
Federico Viera <fedviera@gmail.com>
2024-06-04 14:55:54 -0300

    Fixed outB, sound works :DDDD

--- Kernel/asm/libasm.asm
+++ Kernel/asm/libasm.asm
@@ -58,17 +58,21 @@ rtc:
 inb:
 	push rbp
     mov rbp, rsp
+
 	mov rdx, rdi
-	mov rax, 0
 	in al, dx
+
 	mov rsp, rbp
 	pop rbp
 	ret
 outb:
 	push rbp
     mov rbp, rsp
+
+ 	mov rax, rsi
 	mov rdx, rdi
 	out dx, al
+
 	mov rsp, rbp
 	pop rbp
 	ret
