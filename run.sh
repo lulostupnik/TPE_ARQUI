53999c4c07af3cc14e40d0d11a46337763f77fef
Federico Viera <fedviera@gmail.com>
2024-06-04 22:34:24 -0300

    Update Run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 sudo chmod -R 777 ./*
 sudo docker start TPE_CONTAINER
 sudo docker exec -it TPE_CONTAINER make -C/root/Toolchain
-sudo docker exec -it TPE_CONTAINER make -C/root/ > make_out.txt
+sudo docker exec -it TPE_CONTAINER make -C/root/
 sudo chmod 777 Image/x64BareBonesImage.qcow2
 #qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512
 qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker -machine pcspk-audiodev=speaker
 sudo docker exec -it TPE_CONTAINER make clean -C/root/Toolchain
 sudo docker exec -it TPE_CONTAINER make clean -C/root/
-sudo docker stop TPE_CONTAINER                                             #stop
+sudo docker stop TPE_CONTAINER
